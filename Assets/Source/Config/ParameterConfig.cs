519b2126fdd92f08cb56aefb1d90b29ff3fb168b
twsqrt <twsqrt@gmail.com>
2023-09-01 19:29:20 +0300

    Changed parameter config

--- Assets/Source/Config/ParameterConfig.cs
+++ Assets/Source/Config/ParameterConfig.cs
@@ -3,19 +3,19 @@ using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace config
+namespace Config
 {
     [CreateAssetMenu(fileName = "ParameterConfig", menuName = "Config/ParameterConfig", order = 51)]
     public class ParameterConfig : ScriptableObject
     {
         [Serializable]
-        private class ParameterName
+        private class ParameterInfo
         {
             public int Id;
             public string Name; 
         }
 
-        [SerializeField] private List<ParameterName> _names;
+        [SerializeField] private List<ParameterInfo> _names;
 
         public Dictionary<int, string> ToDictionary()
             => _names.ToDictionary(n => n.Id, n => n.Name);
