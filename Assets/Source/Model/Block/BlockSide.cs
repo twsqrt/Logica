268436968877b95df00d506cdb8d4d6e78520e4d
twsqrt <twsqrt@gmail.com>
2023-09-11 22:47:26 +0300

    Added flags to 'BlockSide'

--- Assets/Source/Model/Block/BlockSide.cs
+++ Assets/Source/Model/Block/BlockSide.cs
@@ -1,11 +1,18 @@
+using System;
+
 namespace Model.BlockLogic
 {
+    [Flags]
     public enum BlockSide
     {
         NONE = 0,
-        UP,
-        DOWN,
-        LEFT,
-        RIGHT,
+        UP = 1,
+        DOWN = 2,
+        LEFT = 4,
+        RIGHT = 8,
+
+        VERTICALLY = 3,
+        HORIZONTALLY = 12,
+        ALL = 15,
     }
 }
