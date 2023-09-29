bdd95d5e53321f97b99a92c9e3a613b69b8f56be
twsqrt <twsqrt@gmail.com>
2023-09-29 15:25:24 +0300

    Changed parameters conifg

--- Assets/Source/Config/ParametersConfig.cs
+++ Assets/Source/Config/ParametersConfig.cs
@@ -1,22 +1,18 @@
 using System.Collections.Generic;
 using System.Linq;
+using Newtonsoft.Json;
 
 namespace Config
 {
     public class ParametersConfig
     {
-        private readonly IReadOnlyCollection<ParameterConfig> _parameters;
+        [JsonProperty("parameters")] private ParameterConfig[] _parameters;
 
         public int NumberOfParameters 
             => _parameters.Count();
         
         public IEnumerable<int> ParametersId
             => _parameters.Select(p => p.Id);
-
-        public ParametersConfig(IReadOnlyCollection<ParameterConfig> parameters)
-        {
-            _parameters = parameters;
-        }
 
         public Dictionary<int, string> ToNameDictionary()
             => _parameters.ToDictionary(p => p.Id, p => p.Name);
