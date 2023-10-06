c252ae4846da63e706614b472362a88861b286f4
twsqrt <twsqrt@gmail.com>
2023-10-06 18:59:04 +0300

    Added level state class

---
+++ Assets/Source/Model/LevelState/LevelState.cs
@@ -0,0 +1,24 @@
+using Model.InventoryLogic;
+using Model.MapLogic;
+using Model.TreeLogic;
+
+namespace Model.LevelStateLogic
+{
+    public class LevelState
+    {
+        private readonly Map _map; 
+        private readonly Inventory _inventory;
+        private readonly BlockTree _tree;
+
+        public LevelState(Map map, Inventory inventory, BlockTree tree) 
+        {
+            _map = map;
+            _inventory = inventory;
+            _tree = tree;
+        }
+
+        public ReadOnlyMap Map => _map.AsReadOnly();
+        public Inventory Inventory => _inventory;
+        public BlockTree Tree => _tree;
+    }
+}
